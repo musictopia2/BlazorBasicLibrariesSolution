b63d3a4c7b3928ecbacbc95e0e84a06a4219597a
Andy <musictopia2@hotmail.com>
2021-11-12 14:21:35 -0700

    Service Extensions To Register Date Pickers. Also, updated to new updated version of common libraries to do the bug fix for that one.

---
+++ BasicBlazorLibrary/Helpers/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using CommonBasicLibraries.BasicDateTimeProcesses;
+using Microsoft.Extensions.DependencyInjection;
+namespace BasicBlazorLibrary.Helpers;
+public static class ServiceCollectionExtensions
+{
+    public static ServiceCollection RegisterRealDatePicker(this ServiceCollection services)
+    {
+        services.AddSingleton<IDatePicker, RealDatePicker>();
+        services.AddSingleton<IDateOnlyPicker, RealDatePicker>();
+        return services;
+        //this means i can do mocks if necessary.
+    }
+    public static IServiceCollection RegisterRealDatePicker(this IServiceCollection services)
+    {
+        services.AddSingleton<IDatePicker, RealDatePicker>();
+        services.AddSingleton<IDateOnlyPicker, RealDatePicker>();
+        return services;
+    }
+}
