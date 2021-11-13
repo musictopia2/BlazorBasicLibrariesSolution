4984f41c4da15f0803d7751bebd56dde7ec0d2ce
Andy <musictopia2@hotmail.com>
2021-11-12 20:26:52 -0700

    Highlight Javascript Class Completed.

---
+++ BasicBlazorLibrary/BasicJavascriptClasses/HighlightTextBoxClass.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicBlazorLibrary.BasicJavascriptClasses;
+public class HighlightTextBoxClass : BaseLibraryJavascriptClass
+{
+    public HighlightTextBoxClass(IJSRuntime js) : base(js) { }
+    protected override string JavascriptFileName => "highlighter";
+    public async Task PartialHighlightText(ElementReference? element, int startat)
+    {
+        await ModuleTask.InvokeVoidFromClassAsync("highlighttext", element, startat);
+    }
+}
---
+++ BasicBlazorLibrary/wwwroot/highlighter.js
@@ -0,0 +1,5 @@
+﻿export function highlighttext(element, startat) {
+    console.log(element.value);
+    element.setSelectionRange(startat, 100000);
+    element.focus(); //this time, use the javascript focus method.
+}
