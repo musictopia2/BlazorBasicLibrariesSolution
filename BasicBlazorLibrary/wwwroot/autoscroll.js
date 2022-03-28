69d2ef2542d356f2fc61ae1424767de3ad35b081
Andy Walter <musictopia2@hotmail.com>
2022-03-28 11:17:13 -0700

    Fix to textarea. Also, fixed the autoscrolling because of browser updates via javascript.

--- BasicBlazorLibrary/wwwroot/autoscroll.js
+++ BasicBlazorLibrary/wwwroot/autoscroll.js
@@ -1,6 +1,33 @@
-﻿export function scrolltoelement(element) {
+﻿var wasrippedoff = true;
+function start() {
+    document.onkeydown = function (evt) {
+        if (evt == null) evt = event; if (testKeyCode(evt, 38)) //up arrow     
+        {
+            cancelKeyEvent(evt); //up arrow can't do default action which is scroll
+            return false;
+        }
+        if (evt == null) evt = event; if (testKeyCode(evt, 40)) //down arrow     
+        {
+            cancelKeyEvent(evt); //down arrow can't do default action which is scroll.
+            return false;
+        }
+        function cancelKeyEvent(evt) {
+            if (window.createPopup) evt.keyCode = 0;
+            else evt.preventDefault();
+        }
+        function testKeyCode(evt, intKeyCode) {
+            if (window.createPopup) return evt.keyCode == intKeyCode;
+            else return evt.which == intKeyCode;
+        }
+    }
+    wasrippedoff = false; //no longer ripped off because now the arrow up and arrow down won't do default functions which is scroll up/down (since i am handling it via code)
+}
+export function scrolltoelement(element) {
     if (element == undefined || element == null) {
         return;
+    }
+    if (wasrippedoff) {
+        start();
     }
     element.scrollIntoView();
 }
