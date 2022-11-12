78e211c624d7f349ead5eafb5c87ed126c2dc637
Andy Walter <musictopia2@hotmail.com>
2022-11-12 14:05:55 -0700

    Added the basic table stuff without relying on bootstrap.

--- BasicBlazorLibrary/Components/Basic/BasicStyleComponent.razor.cs
+++ BasicBlazorLibrary/Components/Basic/BasicStyleComponent.razor.cs
@@ -4,7 +4,13 @@ public partial class BasicStyleComponent
     [Parameter]
     public RenderFragment? ChildContent { get; set; }
     public const string Btn = "btn"; //this will help with intellisense for different class names.
+
     public const string NoSelect = "usernoselect";
     public const string TestRed = "testred";
     public const string Bold = "bold";
+    public const string Table = "table";
+    public const string TableStriped = "table-striped";
+    public const string TheadDark = "thead-dark";
+    //this will be all the stuff from either bootstrap i really like or would be any generic classes i really want.
+    //if i do generic classes, then i have to know what to name it so its easy to use.
 }
