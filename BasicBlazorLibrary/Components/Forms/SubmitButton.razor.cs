074fcef32d76b5c45af79411115f0e005840e7d5
Andy Walter <musictopia2@hotmail.com>
2022-11-15 20:27:57 -0700

    Added ability to make the submit button primary. Most of the time, will be primary button.

--- BasicBlazorLibrary/Components/Forms/SubmitButton.razor.cs
+++ BasicBlazorLibrary/Components/Forms/SubmitButton.razor.cs
@@ -6,4 +6,6 @@ public partial class SubmitButton
 
     [Parameter]
     public RenderFragment? ChildContent { get; set; }
+    [Parameter]
+    public bool IsPrimary { get; set; } = true; //if true, then use the special primary button.  usually will be primary button.
 }
