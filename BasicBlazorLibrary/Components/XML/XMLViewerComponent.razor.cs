0cd71ddca54213221ba833b337cb59f8142f2c7c
Andy Walter <musictopia2@hotmail.com>
2023-07-04 18:19:20 -0700

    Update to xml viewer. Now allows it to be clickable. Plus even allows flexibility for width as well.

--- BasicBlazorLibrary/Components/XML/XMLViewerComponent.razor.cs
+++ BasicBlazorLibrary/Components/XML/XMLViewerComponent.razor.cs
@@ -6,4 +6,18 @@ public partial class XMLViewerComponent
     public XElement? Element { get; set; }
     [Parameter]
     public string Height { get; set; } = "100%";
+    [Parameter]
+    public string Width { get; set; } = "100%";
+    [Parameter]
+    public EventCallback OnClick { get; set; }
+    private async Task PrivateClickAsync()
+    {
+        if (OnClick.HasDelegate == false)
+        {
+            return;
+        }
+        await OnClick.InvokeAsync();
+    }
+    private static string GetBackgroundColor => cc1.White.ToWebColor();
+    private string GetHoverColor => OnClick.HasDelegate ? cc1.LightYellow.ToWebColor() : GetBackgroundColor;
 }
