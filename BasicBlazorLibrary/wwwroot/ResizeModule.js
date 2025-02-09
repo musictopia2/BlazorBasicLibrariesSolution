9dd03a7b1b9bfff493a9eaffdf498dc36fe941bc
Andy Walter <musictopia2@hotmail.com>
2025-02-09 09:20:01 -0700

    Simplify window resize handling using screen dimensions
    The `resizeHandler` method now uses `window.screen.width` and
    `window.screen.height` instead of `window.innerHeight` and
    `window.innerWidth` when invoking the `RaiseOnResized` method.
    
    The `getBrowserWindowSize` method was simplified by removing
    the logic that calculated the adjusted width and height based
    on the zoom factor. It now directly returns the screen's width
    and height using `window.screen.width` and `window.screen.height`.
    
    These changes simplify the handling of window resize events by
    removing the need to account for zoom factors.

--- BasicBlazorLibrary/wwwroot/ResizeModule.js
+++ BasicBlazorLibrary/wwwroot/ResizeModule.js
@@ -4,8 +4,8 @@
     resizeHandler = () => {
         this.dotnet.invokeMethodAsync(
             'RaiseOnResized', {
-            height: window.innerHeight,
-            width: window.innerWidth
+                height: window.screen.width,
+                width: window.screen.height
         });
     }
     listenForResize(dotnetRef) {
@@ -21,25 +21,9 @@
         window.removeEventListener("resize", this.throttleResizeHandler);
     }
     getBrowserWindowSize() {
-        // Get the initial inner width and height of the window
-        const width = window.innerWidth;
-        const height = window.innerHeight;
-
-        // Get the screen's physical size
-        const screenWidth = window.screen.width;
-        const screenHeight = window.screen.height;
-
-        // Check if zoom is applied by comparing screen width and inner width
-        const zoomFactor = screenWidth / width;
-
-        // If zoom is applied, adjust the width and height accordingly
-        const adjustedWidth = width * zoomFactor;
-        const adjustedHeight = height * zoomFactor;
-
-        // Return the adjusted width and height
         return {
-            width: adjustedWidth,
-            height: adjustedHeight
+            width: window.screen.width,
+            height: window.screen.height
         };
     }
 }
