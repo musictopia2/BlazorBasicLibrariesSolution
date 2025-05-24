05c243c5bdb101aa42df4620f1b48151313074e2
Andy Walter <musictopia2@hotmail.com>
2025-05-24 10:11:07 -0700

    Added scroll helpers into the basic library since putting into windows only causes the javascript to not get loaded.

---
+++ BasicBlazorLibrary/BasicJavascriptClasses/ScrollHelperClass.cs
@@ -0,0 +1,17 @@
+﻿namespace BasicBlazorLibrary.BasicJavascriptClasses;
+internal class ScrollHelperClass(IJSRuntime js) : BaseLibraryJavascriptClass(js)
+{
+    protected override string JavascriptFileName => "scrollhelpers.js";
+    public async Task ScrollDownOnePage(ElementReference element, int buffer = 100)
+    {
+        await ModuleTask.InvokeVoidFromClassAsync("scrollDownOnePage", element, buffer);
+    }
+    public async Task ScrollUpOnePage(ElementReference element, int buffer = 100)
+    {
+        await ModuleTask.InvokeVoidFromClassAsync("scrollUpOnePage", element, buffer);
+    }
+    public async Task ScrollToTop(ElementReference element, bool smooth = false)
+    {
+        await ModuleTask.InvokeVoidFromClassAsync("scrollToTop", element, smooth);
+    }
+}
