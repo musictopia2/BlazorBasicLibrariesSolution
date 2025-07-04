19e72c7f8840283efbfe85b67a9b38e0dff682d2
Andy Walter <musictopia2@hotmail.com>
2025-07-04 08:20:48 -0700

    Add UseBlazorAssetMappingFormat parameter for CSS files
    Introduced a new parameter `UseBlazorAssetMappingFormat` in
    `CssSimpleSetup` and `FullCssSetup` components to control
    how CSS files are referenced. Updated rendering logic to
    conditionally use original or mapped asset paths based on
    this parameter. Ensured both components support the new
    functionality for improved asset management.

--- BasicBlazorLibrary/Components/CssManagement/CssSimpleSetup.razor.cs
+++ BasicBlazorLibrary/Components/CssManagement/CssSimpleSetup.razor.cs
@@ -5,6 +5,9 @@ public partial class CssSimpleSetup
     public string AppStyleName { get; set; } = "";
 
     [Parameter]
+    public bool UseBlazorAssetMappingFormat { get; set; } = false;
+
+    [Parameter]
     public BasicList<string> CssFiles { get; set; } = [];
     [Parameter]
     public RenderFragment? ChildContent { get; set; }
--- BasicBlazorLibrary/Components/CssManagement/FullCssSetup.razor.cs
+++ BasicBlazorLibrary/Components/CssManagement/FullCssSetup.razor.cs
@@ -4,4 +4,11 @@ public partial class FullCssSetup
     [Parameter]
     public string AppStyleName { get; set; } = "";
     private string ScopedName => $"{AppStyleName}.styles.css";
+
+
+
+    [Parameter]
+    public bool UseBlazorAssetMappingFormat { get; set; } = false;
+
+
 }
