2ee341ed687d1bbe4eb8eb1a0bd91babec22e4f3
Andy Walter <musictopia2@hotmail.com>
2025-10-22 15:07:07 -0700

    Simplify ResourceImage and update dependencies
    Updated CommonBasicLibraries to version 9.0.78 and removed the Musictopia.AssemblyHelpersLibrary package. Simplified the ResourceImage component by removing the Assembly parameter and its associated logic. Refactored the GetLink method to use FileContentRegistry for file retrieval, reducing complexity and dependencies. Improved maintainability and streamlined the component's API.

--- BasicBlazorLibrary/Components/Basic/ResourceImage.razor.cs
+++ BasicBlazorLibrary/Components/Basic/ResourceImage.razor.cs
@@ -1,5 +1,4 @@
-using AssemblyHelpersLibrary.BasicExtensions;
-using CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.FileFunctions;
+using ff2 = CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.FileFunctions.FileContentRegistry;
 namespace BasicBlazorLibrary.Components.Basic;
 public partial class ResourceImage
 {
@@ -20,22 +19,9 @@ public partial class ResourceImage
     public string FileName { get; set; } = "";
     [Parameter]
     public bool Fixed { get; set; } = true;
-    [Parameter]
-    [EditorRequired]
-    public Assembly? Assembly { get; set; }
     private string GetLink()
     {
-        string text;
-        text = Assembly!.ResourcesBinaryTextFromFile(FileName);
-        if (FileName.ToLower().EndsWith(".svg"))
-        {
-            return $"data:image/svg+xml;base64,{text}";
-        }
-        if (FileName.ToLower().EndsWith(".png") == false)
-        {
-            throw new CustomBasicException("Only pngs or svg are supported for now");
-        }
-        return $"data:image/png;base64,{text}";
+        return ff2.GetFile(FileName);
     }
     protected override bool ShouldRender()
     {
