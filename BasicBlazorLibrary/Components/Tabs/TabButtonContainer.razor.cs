a58e7212556398a142a22afa10b3be7d46505eb7
Andy Walter <musictopia2@hotmail.com>
2026-01-13 19:13:14 -0700

    Remove TabButtonContainer.razor.cs file and its logic
    Deleted the entire TabButtonContainer.razor.cs file, including the GetButtonClass method and related class/namespace definitions, as part of code cleanup or refactoring.

--- BasicBlazorLibrary/Components/Tabs/TabButtonContainer.razor.cs
+++
@@ -1,8 +0,0 @@
-namespace BasicBlazorLibrary.Components.Tabs;
-public partial class TabButtonContainer
-{
-    private string GetButtonClass(TabPage page)
-    {
-        return page == ActivePage ? "btn-primary" : "btn-secondary";
-    }
-}
